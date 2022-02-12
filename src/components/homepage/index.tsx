6895d50945c68913f5d47b3a014d92dda9618b32
Eric Wu <ericwu692@yahoo.com>
2022-02-12 13:45:17 -0500

    formatting

--- src/components/homepage/index.tsx
+++ src/components/homepage/index.tsx
@@ -7,13 +7,13 @@ export const Homepage = (): JSX.Element => {
             <h1> Looking for a Home? </h1>
             <ImageCarousel />
             <h1> About Us </h1>
-            <p>
+            <p className="homepage-text">
                 At 888 Holdings, the management team for Mystic Village
                 Apartments, we have brought together a highly effective team
                 that has in-depth experience in all aspects of real estate and
                 property management.
             </p>
-            <p>
+            <p className="homepage-text">
                 In Mystic Village Apartments, we provide extraordinary value and
                 superior service to our tenants and Airbnb guests, so they feel
                 at Home.
