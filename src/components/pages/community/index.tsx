1086d69996924f54609b3c5c220f1e06ada000e3
Eric Wu <ericwu692@yahoo.com>
2022-02-15 08:38:19 -0500

    community

--- src/components/pages/community/index.tsx
+++ src/components/pages/community/index.tsx
@@ -1,5 +1,55 @@
 import React from 'react';
+import outside from './../../../assets/images/outside.jpg';
 
-export const Community = (): JSX.Element => (
-    <div className={'Community'}> HELLO Community ? </div>
-);
+export const Community = (): JSX.Element => {
+    return (
+        <div className={'Community'}>
+            <img
+                src={outside}
+                width={'45%'}
+                style={{
+                    display: 'flex',
+                    margin: '40px auto',
+                    borderRadius: '10px',
+                }}
+                alt={'Image of Outside of Apartment'}
+            />
+
+            <h1> Our Neighborhood </h1>
+            <h3 className="homepageText">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum
+            </h3>
+            <h1>About Mystic</h1>
+            <h3 className="homepageText">
+                Mystic is a charming village on Connecticut's southeast
+                coastline. The city revolves around boating and is home to an
+                array of marinas and historic landmarks. Residents and visitors
+                alike enjoy visiting the family-friendly Mystic Seaport Museum.
+                This reimagined settlement on the Mystic River features a
+                19th-century village, historic chips, a museum, and more. The
+                central commercial district in Mystic is lined with local
+                restaurants and specialty shops like the Engine Room, a
+                rustic-chic eatery offering classic comfort food and craft
+                beers. Across the river you'll find even more dining options and
+                local shops. Residents flock to Williams Beach Park to enjoy the
+                seasonal benefits to this green space that offers swimming, a
+                playground, sports fields, and scenic picnic spots overlooking
+                the water.
+            </h3>
+            <h1>Highlights</h1>
+            <h3 className="homepageText">
+                <ul style={{ listStyle: 'none' }}>
+                    <li> Bowling </li>
+                    <li> Swimming </li>
+                    <li> Food </li>
+                </ul>
+            </h3>
+        </div>
+    );
+};
--- src/components/pages/homepage/index.tsx
+++ src/components/pages/homepage/index.tsx
@@ -7,13 +7,13 @@ export const Homepage = (): JSX.Element => {
             <h1> Looking for a Home? </h1>
             <ImageCarousel />
             <h1> About Us </h1>
-            <p className={'homepage-text'}>
+            <p className={'homepageText'}>
                 At 888 Holdings, the management team for Mystic Village
                 Apartments, we have brought together a highly effective team
                 that has in-depth experience in all aspects of real estate and
                 property management.
             </p>
-            <p className={'homepage-text'}>
+            <p className={'homepageText'}>
                 In Mystic Village Apartments, we provide extraordinary value and
                 superior service to our tenants and Airbnb guests, so they feel
                 at Home.
