41d0193e9f127383e4906e272ccf97a084db2dcf
Eric Wu <ericwu692@yahoo.com>
2022-02-17 12:15:24 -0500

    damn im stressed

--- frontend/src/components/navigation/navmenu.tsx
+++ frontend/src/components/navigation/navmenu.tsx
@@ -5,8 +5,8 @@ export const NavMenu = (): JSX.Element => (
     <div className={'menu'}>
         <NavItem name={'Home'} location={'/'} />
         <NavItem name={'Our Community'} location={'community'} />
-        <NavItem name={'Available Apartments'} location={'apartments'} />
-        <NavItem name={'Schedule Tour'} location={'tour'} />
+        <NavItem name={'Available Rentals'} location={'apartments'} />
+        {/* <NavItem name={'Schedule Tour'} location={'tour'} /> */}
         <NavItem name={'Contact Us'} location={'contact'} />
     </div>
 );
