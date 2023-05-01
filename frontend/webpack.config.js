b72c1cb8126a9ad2467402f646024dd2bf67e9a7
Eric-B-Wu <wue@microsoft.com>
2023-05-01 09:13:43 -0400

    please work

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -53,6 +53,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: path.join(__dirname, 'src', 'index.html'),
+            favicon: './src/favicon.ico',
         }),
     ],
 };
