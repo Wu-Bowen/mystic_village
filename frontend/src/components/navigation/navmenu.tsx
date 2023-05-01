4f3a182aa1ebbdb27afa57fc7e0c80148fc120af
Eric-B-Wu <wue@microsoft.com>
2023-05-01 09:06:33 -0400

    small fix

--- frontend/src/components/navigation/navmenu.tsx
+++ frontend/src/components/navigation/navmenu.tsx
@@ -5,16 +5,18 @@ interface NavMenuProps {
     isMobile: boolean;
 }
 
-export const NavMenu = (isMobile: NavMenuProps): JSX.Element => (
-    <div className="menu">
-        {isMobile ? null : (
-            <>
-                <NavItem name="Home" location="/" />
-                <NavItem name="Our Community" location="community" />
-                <NavItem name="Available Rentals" location="apartments" />
-                {/* <NavItem name="Schedule Tour" location="tour" /> */}
-                <NavItem name="Contact Us" location="contact" />
-            </>
-        )}
-    </div>
-);
+export const NavMenu = ({ isMobile }: NavMenuProps): JSX.Element => {
+    return (
+        <div className="menu">
+            {isMobile ? null : (
+                <>
+                    <NavItem name="Home" location="/" />
+                    <NavItem name="Our Community" location="community" />
+                    <NavItem name="Available Rentals" location="apartments" />
+                    {/* <NavItem name="Schedule Tour" location="tour" /> */}
+                    <NavItem name="Contact Us" location="contact" />
+                </>
+            )}
+        </div>
+    );
+};
